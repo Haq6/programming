020437dfc2715e96d07c1f353b1dd8b1255e5cb2
Haq6 <haqm6@uni.coventry.ac.uk>
2016-12-03 00:32:49 +0000

    Add files via upload

---
+++ q8.py
@@ -0,0 +1,18 @@
+Pseudocode
+for l in word:
+    if l not in v:
+        message += l
+
+
+Implementation 
+v = ("a", "e", "i", "o", "u")
+
+word = input("Please enter the word you want remove the vowels : ")
+
+message = ""
+
+for l = word:
+    if l not = v:
+        message += l
+
+out message
