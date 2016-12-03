d0eb6a12549d6608b6b9679a21d699390bf9f840
Haq6 <haqm6@uni.coventry.ac.uk>
2016-12-03 00:24:10 +0000

    Add files via upload

---
+++ qu5.py
@@ -0,0 +1,61 @@
+Addition(B,C) 
+
+s = 0 
+for i = B 
+for j = i 
+d[s] <- j 
+s = s + 1 
+s = 0 
+
+for = C	
+for j =  i 
+d[s] <- d[s] + j 
+s = s + 1 
+
+OUT(d) 
+
+Subtraction(B,C) 
+
+
+s = 0 
+for = B 
+for j = i 
+d[s] <- j 
+s = s + 1 
+
+p = 0 
+for = C
+for j = i
+d[s] <- d[p] - j
+s = s + 1 
+
+OUT(d) 
+
+Multiplication(B,C) 
+
+
+s = 0
+for = B 
+for j = i 
+d[s] <- j 
+s = s + 1 
+
+s = 0
+for i = C 
+for j =  i 
+d[s] <- d[s] * j 
+s = s + 1 
+
+OUT(d) 
+
+
+
+
+addition  runtime 6n^2 + 2n + 3
+subtraction  runtime 6n^2 + 2n + 3
+multiplication runttime 6n^2 + 2n + 3
+
+Run time
+
+24n^2 + 8n + 15
+ O(n*n)
