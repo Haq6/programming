948f2380cf71987405a77d309d9627453389d022
Haq6 <haqm6@uni.coventry.ac.uk>
2016-12-03 00:20:50 +0000

    Add files via upload

---
+++ q3.py
@@ -0,0 +1,21 @@
+Pusdocode 
+
+rnumber <- r(parameter)
+rnumber <- round (rnumber)
+snumber <- rnumber * rnumber
+out snumber
+
+
+Implementation
+
+num = False
+while num == False:
+    try:
+        parameter = int(input("Enter Number: "))
+        num = True
+    except ValueError:
+        print("Only enter positive number.")
+
+rParameter = (parameter)**0.5 
+s = (int(rParameter))**2 
+print(s)
